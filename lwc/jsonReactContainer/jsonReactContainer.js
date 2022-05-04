f9a077cda13b9c850693336de1a787588d138755
Emmanuel Zysman <ezysman@copado.com>
2022-05-04 08:06:10 +0000

    US-0000014: React LWC DEV2 Conflict 2

---
+++ lwc/jsonReactContainer/jsonReactContainer.js
@@ -0,0 +1,18 @@
+/* global moment */
+import { LightningElement, wire, api, track } from "lwc";
+import { loadScript } from "lightning/platformResourceLoader";
+import App from "@salesforce/resourceUrl/app";
+
+export default class JsonReactContainer extends LightningElement {
+  @api jsonText;
+  @api reactJsonViewProps;
+
+  renderedCallback() {
+    Promise.all([loadScript(this, App)]).then(() => {
+      mount(this.template.querySelector("div5"), {
+        jsonText: this.jsonText,
+        reactJsonViewProps: this.reactJsonViewProps
+      });
+    });
+  }
+}
